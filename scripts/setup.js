562e123b2a8a185f5e3c54cf58d2e7c9f40a504b
Bhavika Madhwani <madhwanibhavika123@gmail.com>
2025-03-05 04:30:29 -0800

    Commenting old code to restructure the Files

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,13 +5,7 @@
 */
 
 
-/* Function Name: setup
- * Description: Sets up the initial state of the GenderMag bar with a the passed-in template (which should
- *		be "firstState.html" at this point. Adds onclicks to the two buttons.
- * Parameters: Takes 2 arguements:
- * 		id: the id of the element to which the template will be appended (e.g. '#GenderMargFrame')
- * 		file: the LOCAL path of the template to use (e.g., "/templates/firstState.html")
- */
+
 // function setup(id, file) {
 // 	//Restore the state of the HTML if it exists, and otherwise draw the normal starting state
 // 	var hasStartedWalkthrough = statusIsTrue("startedGM");
@@ -47,34 +41,3 @@
 // 		});
 // 	}
 // }
-function setup(id, file) {
-    // Restore the state of the HTML if it exists, and otherwise draw the normal starting state
-    var hasStartedWalkthrough = statusIsTrue("startedGM");
-    if (hasStartedWalkthrough) {
-        if (statusIsTrue("sliderIsOpen")) {
-            openSlider();
-        } else {
-            closeSlider();
-        }
-        preWalkthrough("#GenderMagFrame", "./templates/popup.html");
-        if (statusIsTrue('drewToolTip')) {
-            reloadToolTipState();
-        }
-    } else {
-        // Put the text and buttons on the screen
-        var el = $(id).contents().find('body');
-        appendTemplateToElement(el, file, function (error) {
-            if (error) {
-                console.error("Error loading body:", error);
-                return;
-            }
-            console.log("Body appended in setup.");
-            
-            // Add the onclick to the "Start Walkthrough" button AFTER the template is appended
-            el.children('#startGenderMagButton').off('click').on('click', function () {
-                setStatusToTrue("startedGM");
-                preWalkthrough("#GenderMagFrame", "./templates/popup.html");
-            });
-        });
-    }
-}
